47084cab283c6deebe84502385c6f6dbabee27fa
Gaurav-00 <shindegaurav5551@gmail.com>
2022-04-09 10:38:43 +0530

    read data from UI and print it

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
 
 app=Flask(__name__)
 
@@ -8,8 +8,21 @@ def StudentDate():
 @app.route("/login")
 def login():
     return render_template("login.html")
-@app.route("/register")
+@app.route("/register", methods=["GET","POST"])
 def register():
+    if request.method=="POST":
+        getName=request.form["name"]
+        getPhoneno=request.form["Phoneno"]
+        getusername=request.form["username"]
+        getemail=request.form["email"]
+        getpassword=request.form["password"]
+        getconfirmpassword=request.form["cpassword"]
+        print(getName)
+        print(getPhoneno)
+        print(getusername)
+        print(getemail)
+        print(getpassword)
+        print(getconfirmpassword)
     return render_template("register.html")
 @app.route("/search")
 def Search_data():
