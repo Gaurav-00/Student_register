6421100cafffc782d286e3894886a403e61a1dc6
Gaurav-00 <shindegaurav5551@gmail.com>
2022-04-09 11:45:12 +0530

    data added to db successfully

--- main.py
+++ main.py
@@ -1,4 +1,17 @@
 from flask import Flask,render_template,request
+import sqlite3
+#to create connection with db
+conn= sqlite3.connect("Studentmanagement.db",check_same_thread=False) #to avoid conflicts use this means storing data in another db instead of this db to avoid such condition
+cursor=conn.cursor()
+#to check wheter table is present or not
+listOfTables=conn.execute("select name from sqlite_master WHERE type='table'AND name='STUDENT'").fetchall()
+
+if listOfTables!=[]:
+    print("table already exists")
+else:
+    conn.execute('''CREATE TABLE STUDENT (id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    NAME TEXT, PHONE_NO INTEGER,USERNAME TEXT,EMAIL TEXT,PASSWORD TEXT,CONFIRM_PASSWORD TEXT);''')
+    print("Table has created")
 
 app=Flask(__name__)
 
@@ -23,6 +36,12 @@ def register():
         print(getemail)
         print(getpassword)
         print(getconfirmpassword)
+        #just inserting values in DB using try ,catch
+        try:
+            conn.execute("INSERT INTO STUDENT(NAME,PHONE_NO,USERNAME,EMAIL,PASSWORD)VALUES ('"+getName+"','"+getPhoneno+"','"+getusername+"','"+getemail+"','"+getpassword+"')")
+            print("Successfully inserted")
+        except Exception as e:
+            print(e)
     return render_template("register.html")
 @app.route("/search")
 def Search_data():
